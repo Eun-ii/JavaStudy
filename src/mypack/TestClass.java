ed5274aa14a141cf3bd363982e4d94997eafbc13
eun.ii <melting.eun.ii@gmail.com>
2023-03-07 23:33:23 +0900

    git 연결 확인

--- src/mypack/TestClass.java
+++ src/mypack/TestClass.java
@@ -2,7 +2,6 @@ package mypack;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello JunHo");
-		System.out.println("Hello JeongEun");
+		System.out.println("git 연결");
 	}
 }
