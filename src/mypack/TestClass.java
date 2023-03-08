3e4def4b44ba37107a148da161fae2b651ddb7c4
eun.ii <melting.eun.ii@gmail.com>
2023-03-08 22:03:43 +0900

    구구단

--- src/mypack/TestClass.java
+++ src/mypack/TestClass.java
@@ -1,7 +1,17 @@
 package mypack;
 
+import java.util.Scanner;
+
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("git 연결");
+		System.out.println("구구단을 외자");
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()) {
+			int i = sc.nextInt();
+			int i2 = sc.nextInt();
+			
+			System.out.println(i * i2);	
+		}
+		sc.close();
 	}
 }
