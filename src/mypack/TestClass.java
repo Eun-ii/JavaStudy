02b59c0ea3d3c0a79fca4cd9e18a822ad92918ec
eun.ii <melting.eun.ii@gmail.com>
2023-03-08 22:13:08 +0900

    파일입력

--- src/mypack/TestClass.java
+++ src/mypack/TestClass.java
@@ -1,17 +1,26 @@
 package mypack;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("구구단을 외자");
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextInt()) {
-			int i = sc.nextInt();
-			int i2 = sc.nextInt();
-			
-			System.out.println(i * i2);	
+		try {
+			System.out.println("구구단을 외자");
+			File file = new File("out.txt");
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextInt()) {
+				int i1 = sc.nextInt();
+				int i2 = sc.nextInt();
+				int result = i1 * i2;
+				
+				System.out.printf("%d x %d = %d\n", i1, i2, result);
+			}
+			sc.close();
 		}
-		sc.close();
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
